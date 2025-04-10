6c280d2bf6fc43793b7b9559d775b69d9fc7bc52
Alexis Vespa <alexis_1722@hotmail.es>
2025-04-09 23:47:55 -0300

    ma

--- src/main/java/com/mycompany/inmobiliaria/resources/config/Conexion.java
+++ src/main/java/com/mycompany/inmobiliaria/resources/config/Conexion.java
@@ -1,4 +1,3 @@
-<<<<<<< OURS
 package com.mycompany.inmobiliaria.resources.config;
 
 import java.sql.Connection;
@@ -16,41 +15,15 @@ public class Conexion {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             cn = DriverManager.getConnection(url, username, password);
-            System.out.println("ConexiÃ³n exitosa!");
+            System.out.println("Conexión exitosa!");
         } catch (ClassNotFoundException e) {
             System.out.println("Error: Driver no encontrado - " + e.getMessage());
             e.printStackTrace();
         } catch (SQLException e) {
-            System.out.println("Error: Fallo en la conexiÃ³n a la base de datos - " + e.getMessage());
+            System.out.println("Error: Fallo en la conexión a la base de datos - " + e.getMessage());
             e.printStackTrace();
         }
         return cn;
     }
 }
-=======
 
-package com.mycompany.inmobiliaria.resources.config;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class Conexion {
-        public static final String username = "root";
-        public static final String password = "rocco0505";
-        public static final String database = "inmobiliaria";
-        public static final String url = "jdbc:mysql://localhost:3306/"+database;
-        
-        public static Connection getConnection(){
-            Connection cn = null;
-            
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                cn = DriverManager.getConnection(url,username,password);
-                System.out.println("Conexion exitosa!");
-            } catch (ClassNotFoundException | SQLException ex) {
-            }
-            return cn;
-        }    
-}
->>>>>>> THEIRS
