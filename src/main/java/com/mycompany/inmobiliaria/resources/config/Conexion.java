f8b5a7fe8185b5dded5a1c5c9d3df8cd6c1265ec
Alexis Vespa <alexis_1722@hotmail.es>
2025-04-09 23:41:56 -0300

    insertando caracteristicas adicionales

--- src/main/java/com/mycompany/inmobiliaria/resources/config/Conexion.java
+++ src/main/java/com/mycompany/inmobiliaria/resources/config/Conexion.java
@@ -1,3 +1,33 @@
+<<<<<<< OURS
+package com.mycompany.inmobiliaria.resources.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    public static final String username = "root";
+    public static final String password = "rocco0505";
+    public static final String database = "inmobiliariagrok";
+    public static final String url = "jdbc:mysql://localhost:3306/" + database;
+
+    public static Connection getConnection() {
+        Connection cn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cn = DriverManager.getConnection(url, username, password);
+            System.out.println("Conexión exitosa!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: Driver no encontrado - " + e.getMessage());
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Error: Fallo en la conexión a la base de datos - " + e.getMessage());
+            e.printStackTrace();
+        }
+        return cn;
+    }
+}
+=======
 
 package com.mycompany.inmobiliaria.resources.config;
 
@@ -23,3 +53,4 @@ public class Conexion {
             return cn;
         }    
 }
+>>>>>>> THEIRS
