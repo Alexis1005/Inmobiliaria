3c9e1a2071545ebf277544fdb9c04f604df36e3c
Alexis Vespa <alexis_1722@hotmail.es>
2025-04-10 22:04:41 -0300

    insertando caracteristicas adicionales

--- src/main/java/com/mycompany/inmobiliaria/modelo/PropiedadesCaracteristicas.java
+++ src/main/java/com/mycompany/inmobiliaria/modelo/PropiedadesCaracteristicas.java
@@ -3,25 +3,10 @@ package com.mycompany.inmobiliaria.modelo;
 public class PropiedadesCaracteristicas {
     private int id_propiedad;
     private int id_caracteristica;
-<<<<<<< HEAD
-
-
-    /**
-     * Constructor para inicializar una relación entre propiedad y característica.
-     *
-     * @param id_propiedad      El identificador único de la propiedad.
-     * @param id_caracteristica El identificador único de la característica.
-     */
-    
-    public PropiedadesCaracteristicas(int id_propiedad, int id_caracteristica) {
-        this.id_propiedad = id_propiedad;
-        this.id_caracteristica = id_caracteristica;
-=======
     private String detalle;
 
     // Constructor vacío
     public PropiedadesCaracteristicas() {
->>>>>>> ruben
     }
 
     // Constructor con parámetros
@@ -54,8 +39,6 @@ public class PropiedadesCaracteristicas {
         this.id_caracteristica = idCaracteristica;
     }
 
-<<<<<<< HEAD
-=======
     public String getDetalle() {
         return detalle;
     }
@@ -63,20 +46,13 @@ public class PropiedadesCaracteristicas {
     public void setDetalle(String detalle) {
         this.detalle = detalle;
     }
->>>>>>> ruben
 
     @Override
     public String toString() {
         return "PropiedadesCaracteristicas{" +
-<<<<<<< HEAD
-                "id_propiedad=" + id_propiedad +
-                ", id_caracteristica=" + id_caracteristica +
-                ", + '\'}";
-=======
                "id_propiedad=" + id_propiedad +
                ", id_paracteristica=" + id_caracteristica +
                ", detalle='" + detalle + '\'' +
                '}';
->>>>>>> ruben
     }
 }
