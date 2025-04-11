14461e84ae3c42c3ec722317ce236fd750954273
Alexis Vespa <alexis_1722@hotmail.es>
2025-04-10 22:11:50 -0300

    insertando caracteristicas adicionales

--- src/main/java/com/mycompany/inmobiliaria/controlador/SubirPropiedadServlet.java
+++ src/main/java/com/mycompany/inmobiliaria/controlador/SubirPropiedadServlet.java
@@ -20,7 +20,6 @@ import jakarta.servlet.http.Part;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.ArrayList;
